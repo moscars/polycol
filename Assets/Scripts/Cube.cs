50f9c58801c6de6e1487e77976c4274d2bf4aaa9
moscars <moscars@kth.se>
2021-02-20 18:33:05 +0100

    added cube with gravity

---
+++ Assets/Scripts/Cube.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    public Vector3 gravityForce = new Vector3(0, -9.82f, 0);
+    public Vector3 force;
+    Vector3 velocity;
+    float mass = 2000;
+    Vector3 acceleration;
+
+    void Start(){
+        force = Vector3.zero;
+        velocity = Vector3.zero;
+        acceleration = Vector3.zero;
+        addForce(gravityForce);
+    }
+
+    void FixedUpdate(){
+        if(transform.position.y <= 0.51f){
+            stopAtButtom();
+        } else{
+            updateVelocity();
+            updatePos();
+            calculateAcceleration();
+        }
+    }
+
+    void stopAtButtom(){
+        acceleration = Vector3.zero;
+        velocity = Vector3.zero;
+        transform.position = new Vector3(transform.position.x, 0.5f, transform.position.z);
+    }
+
+    public void addForce(Vector3 force){
+        this.force += force;
+    }
+
+    public void calculateAcceleration(){
+        acceleration = force / mass;
+    }
+
+    public void updateVelocity(){
+        velocity += acceleration;
+    }
+
+    public void zeroVelocity(){
+        velocity = Vector3.zero;
+    }
+
+    public void zeroAcceleration(){
+        acceleration = Vector3.zero;
+    }
+
+    public void updatePos(){
+        transform.position = transform.position + velocity;
+    }
+    
+}
---
+++ Assets/Scripts/Force.cs
@@ -0,0 +1,18 @@
+﻿
+/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Force
+{
+   Vector3 force;
+
+   public Force(float x, float y, float z){
+       this.force = new Vector3(x, y, z);
+   }
+
+   public Force getForce(){
+       return force;
+   }
+}
+*/
