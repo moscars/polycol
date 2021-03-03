83ffa3948441cc926a51c651b8896208a06b1b68
moscars <moscars@kth.se>
2021-03-03 20:51:10 +0100

    almost complete epa

---
+++ Assets/Scripts/CollisionPoints.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionPoints
+{
+    Vector3 normal;
+    float penetrationDepth;
+    bool hasCollision; 
+    public CollisionPoints(Vector3 normal, float penetrationDepth, bool hasCollision){
+        this.normal = normal;
+        this.penetrationDepth = penetrationDepth;
+        this.hasCollision = hasCollision;
+    }
+}
--- Assets/Scripts/GJK.cs
+++ Assets/Scripts/GJK.cs
@@ -1,19 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class GJK
 {
     public GJK(){}
 
-    public void runGJK(CollisionObj firstCollider, CollisionObj secondCollider){
-      if(gjk(firstCollider, secondCollider)){
+    public CollisionPoints runGJK(CollisionObj firstCollider, CollisionObj secondCollider){
+      (bool gjkRes, CollisionPoints points) g = gjk(firstCollider, secondCollider);
+      if(g.gjkRes){
         firstCollider.changeColorGJK();
         secondCollider.changeColorGJK();
       }
+      return g.points;
     }
 
-    public bool gjk(CollisionObj firstCollider, CollisionObj secondCollider){
+    public (bool, CollisionPoints) gjk(CollisionObj firstCollider, CollisionObj secondCollider){
       Vector3 dir = new Vector3(1, 1, 1);
       Vector3 firstPoint = findMaxPointInConvexHull(dir, firstCollider, secondCollider);
       List<Vector3> SimplexList = new List<Vector3>();
@@ -31,14 +34,15 @@ public class GJK
         Vector3 newPoint = findMaxPointInConvexHull(direction, firstCollider, secondCollider);
         float newDotDir = Vector3.Dot(newPoint, direction);
         if(newDotDir <= 0){
-          return false;
+          CollisionPoints p = new CollisionPoints(Vector3.zero, -1, false);
+          return (false, p);
         }
 
         SimplexList.Insert(2, newPoint);
         SimplexList = NewSimplex(SimplexList);
         if(SimplexList[0] == new Vector3(1, 1, 1)){ // Check the boolean flag
-          EPA(firstCollider, secondCollider, SimplexList);
-          return true;
+          CollisionPoints points = EPA(firstCollider, secondCollider, SimplexList);
+          return (true, points);
         }
       }
     } 
@@ -206,7 +210,7 @@ public class GJK
     }
 
 
-    void EPA(CollisionObj firstCollider, CollisionObj secondCollider, List<Vector3> SimplexList){
+    CollisionPoints EPA(CollisionObj firstCollider, CollisionObj secondCollider, List<Vector3> SimplexList){
 
       List<Vector3> polytope = SimplexList;
       List<int> faces = new List<int>{
@@ -216,17 +220,125 @@ public class GJK
         1, 3, 2
       };
 
-      (List<Vector4> normals, int minFace) g = GetFaceNormals(polytope, faces);
+      (List<Vector4> normals, int minFace) temp = GetFaceNormals(polytope, faces);
+      List<Vector4> normals = temp.normals;
+      int minFace = temp.minFace;
 
+      Vector3 minNormal = Vector3.zero;
+      float minDistance = float.PositiveInfinity;
 
+      while(minDistance == float.PositiveInfinity){
+        float x = normals[minFace].x;
+        float y = normals[minFace].y;
+        float z = normals[minFace].z;
+
+        minNormal = new Vector3(x, y, z);
+        minDistance = normals[minFace].w;
+
+        Vector3 furtherPoint = findMaxPointInConvexHull(minNormal, firstCollider, secondCollider);
+        float sDistance = Vector3.Dot(minNormal, furtherPoint);
+
+        if(Math.Abs(sDistance - minDistance) > 0.001f){
+          minDistance = float.PositiveInfinity;
+          List<(int, int)> uniqueEdges = new List<(int, int)>();
+
+          for(int i = 0; i < normals.Count; i++){
+            if(SameDir(normals[i], furtherPoint)){
+              int f = i * 3;
+
+              uniqueEdges = AddIfUniqueEdge(uniqueEdges, faces, f, f + 1);
+              uniqueEdges = AddIfUniqueEdge(uniqueEdges, faces, f + 1, f + 2);
+              uniqueEdges = AddIfUniqueEdge(uniqueEdges, faces, f + 2, f);
+
+              faces[f + 2] = faces[faces.Count - 1];
+              faces.RemoveAt(faces.Count - 1);
+              faces[f + 1] = faces[faces.Count - 1];
+              faces.RemoveAt(faces.Count - 1);
+              faces[f] = faces[faces.Count - 1];
+              faces.RemoveAt(faces.Count - 1);
+
+              normals[i] = normals[normals.Count - 1];
+              normals.RemoveAt(normals.Count - 1);
+
+              i--;
+            }
+          }
+          List<int> newFaces = new List<int>();
+          foreach ((int first, int second) edge in uniqueEdges){
+            newFaces.Add(edge.first);
+            newFaces.Add(edge.second);
+            newFaces.Add(polytope.Count);
+          }
+
+          polytope.Add(furtherPoint);
+          (List<Vector4> newNormals, int newMinFace) temp2 = GetFaceNormals(polytope, newFaces);
+          List<Vector4> newNormals = temp2.newNormals;
+          int newMinFace = temp2.newMinFace;
+
+          float oldMinDistance = float.PositiveInfinity;
+
+          for(int i = 0; i < normals.Count; i++){
+            if(normals[i].w < oldMinDistance){
+              oldMinDistance = normals[i].w;
+              minFace = i;
+            }
+          }
+
+          if(newNormals[newMinFace].w < oldMinDistance){
+            minFace = newMinFace + normals.Count;
+          }
+
+          foreach (int face in newFaces){
+            faces.Add(face);
+          }
+          foreach (Vector4 normal in newNormals){
+            normals.Add(normal);
+          } 
+        }  
+      }
+    CollisionPoints points = new CollisionPoints(minNormal, minDistance + 0.001f, true);
+    return points;
     }
+
+
+
+
+
+
+
+
+    List<(int, int)> AddIfUniqueEdge(List<(int, int)> edges, List<int> faces, int a, int b){
+    
+      bool found = false;
+      bool keepGoing = true;
+      (int, int) reversed = (faces[b], faces[a]);
+      int index = 0;
+
+      for(int i = 0; i < edges.Count; i++){
+        (int, int) elem = edges[i];
+        if(elem == reversed){
+          found = true;
+          index = i;
+        }
+      }
+
+      if(found){
+        edges.RemoveAt(index); 
+      } else{
+        edges.Add((faces[a], faces[b]));
+      }
+
+      return edges;
+    }
+
+
 
     (List<Vector4>, int) GetFaceNormals(List<Vector3> polytope, List<int> faces){
       List<Vector4> normals = new List<Vector4>();
       int minTriangle = 0;
       float minDistance = float.PositiveInfinity;
 
-      for(int i = 0; i < faces.Count; i+=3){
+      for(int i = 0; i < faces.Count; i += 3){
         Vector3 A = polytope[faces[i]];
         Vector3 B = polytope[faces[i + 1]];
         Vector3 C = polytope[faces[i + 2]];
@@ -239,8 +351,8 @@ public class GJK
         float distance = Vector3.Dot(normal, A);
 
         if (distance < 0){
-          normal = -normal;
-          distance = -distance;
+          normal *= -1;
+          distance *= -1;
         }
 
         Vector4 newNormal = new Vector4(normal.x, normal.y, normal.z, distance);
